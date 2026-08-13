071b921908b85648c27704132ac2422ab6f52cec
SⱩUⱠÐ <skuldexter@gmail.com>
2026-08-13 12:54:11 +0200

    Update vuln_agent.py

--- vuln_agent.py
+++ vuln_agent.py
@@ -153,8 +153,8 @@ class VulnerabilityScanner:
         #Example: searchsploit tar
         get IP wont work; we need service. Use nmap output.
         # In real usage we would parse nmap services and search by service:version
-        Utils.print_info("Note: searchsploit requires service info. Using generic searc
-h.")
+        Utils.print_info(Note: searchsploit requires service info. Using generic searc
+h.)
         Utils.run_live(command)
 
     def auto_scan(self):
